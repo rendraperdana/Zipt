fdce63d007593bf2e4e8acff09c4fa032d033f52
Rendra Perdana <rendra.satria@gdn-commerce.com>
2021-02-13 23:22:11 +0700

    First Commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,129 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.btnGetFile = New System.Windows.Forms.Button()
+        Me.btnZip = New System.Windows.Forms.Button()
+        Me.BindingSource1 = New System.Windows.Forms.BindingSource(Me.components)
+        Me.dgvFileList = New System.Windows.Forms.DataGridView()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.gbFileList = New System.Windows.Forms.GroupBox()
+        Me.lblFileCount = New System.Windows.Forms.Label()
+        CType(Me.BindingSource1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.dgvFileList, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.gbFileList.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'btnGetFile
+        '
+        Me.btnGetFile.Location = New System.Drawing.Point(511, 38)
+        Me.btnGetFile.Name = "btnGetFile"
+        Me.btnGetFile.Size = New System.Drawing.Size(237, 74)
+        Me.btnGetFile.TabIndex = 0
+        Me.btnGetFile.Text = "Get File"
+        Me.btnGetFile.UseVisualStyleBackColor = True
+        '
+        'btnZip
+        '
+        Me.btnZip.Location = New System.Drawing.Point(823, 38)
+        Me.btnZip.Name = "btnZip"
+        Me.btnZip.Size = New System.Drawing.Size(237, 74)
+        Me.btnZip.TabIndex = 1
+        Me.btnZip.Text = "Zip-and-Encrypt"
+        Me.btnZip.UseVisualStyleBackColor = True
+        '
+        'dgvFileList
+        '
+        Me.dgvFileList.AllowUserToAddRows = False
+        Me.dgvFileList.AllowUserToDeleteRows = False
+        Me.dgvFileList.AllowUserToOrderColumns = True
+        Me.dgvFileList.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.DisplayedCells
+        Me.dgvFileList.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells
+        Me.dgvFileList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvFileList.Location = New System.Drawing.Point(6, 100)
+        Me.dgvFileList.Name = "dgvFileList"
+        Me.dgvFileList.ReadOnly = True
+        Me.dgvFileList.RowHeadersVisible = False
+        Me.dgvFileList.RowHeadersWidth = 82
+        Me.dgvFileList.RowTemplate.Height = 41
+        Me.dgvFileList.Size = New System.Drawing.Size(1534, 740)
+        Me.dgvFileList.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(17, 44)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(128, 32)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "File Count:"
+        '
+        'gbFileList
+        '
+        Me.gbFileList.Controls.Add(Me.lblFileCount)
+        Me.gbFileList.Controls.Add(Me.Label1)
+        Me.gbFileList.Controls.Add(Me.dgvFileList)
+        Me.gbFileList.Location = New System.Drawing.Point(12, 132)
+        Me.gbFileList.Name = "gbFileList"
+        Me.gbFileList.Size = New System.Drawing.Size(1546, 846)
+        Me.gbFileList.TabIndex = 2
+        Me.gbFileList.TabStop = False
+        '
+        'lblFileCount
+        '
+        Me.lblFileCount.AutoSize = True
+        Me.lblFileCount.Location = New System.Drawing.Point(151, 44)
+        Me.lblFileCount.Name = "lblFileCount"
+        Me.lblFileCount.Size = New System.Drawing.Size(84, 32)
+        Me.lblFileCount.TabIndex = 3
+        Me.lblFileCount.Text = "          "
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(13.0!, 32.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1570, 990)
+        Me.Controls.Add(Me.gbFileList)
+        Me.Controls.Add(Me.btnZip)
+        Me.Controls.Add(Me.btnGetFile)
+        Me.MaximizeBox = False
+        Me.Name = "frmMain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Zip-and-Encrypt"
+        CType(Me.BindingSource1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.dgvFileList, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.gbFileList.ResumeLayout(False)
+        Me.gbFileList.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnGetFile As Button
+    Friend WithEvents btnZip As Button
+    Friend WithEvents BindingSource1 As BindingSource
+    Friend WithEvents dgvFileList As DataGridView
+    Friend WithEvents Label1 As Label
+    Friend WithEvents gbFileList As GroupBox
+    Friend WithEvents lblFileCount As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,97 @@
+﻿Imports System.IO
+Imports System.Security.Cryptography
+Public Class frmMain
+    Public strAppPath As String = Application.StartupPath.ToString
+    Dim dtList As New DataTable
+    Public chrCharSet() As Char = {"A", "B", "C", "D", "E", "F", "G", "H", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")"}
+
+    Private Sub btnGetFile_Click(sender As Object, e As EventArgs) Handles btnGetFile.Click
+        Try
+            dgvFileList.Rows.Clear()
+            dtList.Clear()
+            Call subGetFile()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub btnZip_Click(sender As Object, e As EventArgs) Handles btnZip.Click
+        Try
+            Call subZipEncrypt()
+            Call SaveToCSV(Me.strAppPath & "PasswordPair.txt")
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        dgvFileList.Columns.Add("clmFileList", "File Name")
+        dgvFileList.Columns.Add("clmPassword", "Password")
+        dtList.Columns.Add("FilePath")
+        dtList.Columns.Add("Password")
+    End Sub
+
+    Private Function PasswordCreator() As String
+        Dim rngSeed As New Random
+        Dim strRandom As String = ""
+
+        For i = 0 To 13 Step 1
+            strRandom &= chrCharSet(rngSeed.Next(1, 44))
+        Next
+
+        Return strRandom
+    End Function
+
+    Private Sub SaveToCSV(strFilePath As String)
+        Dim swWriter As New StreamWriter(strFilePath)
+
+        For i = 0 To dtList.Columns.Count - 1
+            swWriter.Write(dtList.Columns(i).ColumnName)
+            If i < dtList.Columns.Count - 1 Then
+                swWriter.Write(",")
+            Else
+                swWriter.WriteLine("")
+            End If
+        Next
+
+        For Each drListRow As DataRow In dtList.Rows
+            swWriter.Write(drListRow(0).ToString)
+            swWriter.Write(",")
+            swWriter.Write(drListRow(1).ToString)
+            swWriter.WriteLine("")
+        Next
+
+        swWriter.Close()
+    End Sub
+
+    Private Sub subZipEncrypt()
+        Try
+            For Each drFileList As DataRow In dtList.Rows
+                Dim zipFile As New Ionic.Zip.ZipFile
+
+                zipFile.Password = drFileList(1).ToString
+                zipFile.Encryption = Ionic.Zip.EncryptionAlgorithm.WinZipAes128
+                zipFile.CompressionLevel = Ionic.Zlib.CompressionLevel.BestSpeed
+                zipFile.AddFile(drFileList(0).ToString, "")
+                zipFile.Save(drFileList(0).ToString + ".zip")
+            Next
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub subGetFile()
+        Dim strList() As String
+        Try
+            For Each foundFile As String In IO.Directory.GetFiles(strAppPath, "*.pdf", IO.SearchOption.TopDirectoryOnly)
+                strList = {foundFile, PasswordCreator()}
+                dtList.Rows.Add(strList)
+                dgvFileList.Rows.Add(strList)
+            Next
+
+            lblFileCount.Text = dtList.Rows.Count
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+End Class
